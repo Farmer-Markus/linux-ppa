7cda349ec9ea2417d44fff7a89225a01810b934d
Your Name <markus@pering.de>
2024-05-16 22:15:09 +0200

    update

---
+++ rebuild.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+dpkg-scanpackages --multiversion . > Packages
+gzip -k -f Packages
+apt-ftparchive release . > Release
+
+echo "Enter email for updating repository!"
+read email
+
+gpg --default-key $email -abs -o - Release > Release.gpg
+gpg --default-key $email --clearsign -o - Release > InRelease
+
+git add -A
+git commit -m update
+git push -u origin HEAD:master
